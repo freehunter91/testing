0a9e868e00951281ea10d49fa49a74a1ca943c50
freehunter91 <chronos@kakao.com>
2022-09-16 17:44:07 +0900

    소스 추가

---
+++ src/main/java/com/test1/webapp/testing/SampleController.java
@@ -0,0 +1,22 @@
+package com.test1.webapp.testing;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableAutoConfiguration
+public class SampleController {
+    @RequestMapping(value="/")
+    @ResponseBody
+    public String sampleHome() {
+        return "Hello Spring Boot";
+    }
+
+    @RequestMapping(value="/health")
+    @ResponseBody
+    public String sampleHealth() {
+        return "OK";
+    }
+}
